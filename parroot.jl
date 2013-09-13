7423209d5cf08df3a92a791832a38a3b8a8a0ac1
Joosep Pata <joosep.pata@gmail.com>
2013-09-13 22:31:24 +0300

    added init

--- parroot.jl
+++ parroot.jl
@@ -1,31 +1,50 @@
-addprocs(3)
-
-require("src/root.jl")
-@everywhere trees = {"file://dat/test.root:trees/Events"}
-@everywhere begin
+function init()
+    require("src/root.jl")
+    @everywhere treepaths = {"hdfs:///local/joosep/test_opt.root:trees/Events"}
+    @everywhere begin
+        results = Dict{ASCIIString, Dict{Symbol, Any}}()
+        trees = Dict{ASCIIString, Tree}()
     
-    results = Dict()
-    for t in trees
-        results[string(t)] = Dict()
-        results[string(t)][:cos_theta] = Histogram(10, -1, 1)
+        for t in treepaths
+            trees[t] = Tree(t)
+            ttree_set_cache(trees[t].tree, 100)
+            results[t] = Dict{Symbol, Any}()
+            results[t][:cos_theta] = Histogram(10, -1, 1)
+        end
+    end
+
+    @everywhere function loopfn(n::Integer, tree::Tree)
+        global results
+    
+        cos_theta::Float32 = tree[:cos_theta, n]
+        if !( cos_theta > -1.0)
+            return false
+        end
+        fill!(results[string(tree)][:cos_theta], cos_theta)
+        return true
     end
 end
 
-@everywhere function loopfn(n::Integer, tree::Tree)
-    global results
-
-    cos_theta::Float32 = tree[:cos_theta, n]
-    fill!(results[string(tree)][:cos_theta], cos_theta)
-    return true
-end
 
 
 function run(nprocs)
-    n, refs = process_parallel(loopfn, "file://dat/test.root:trees/Events")
+    println("Mapping over ", nprocs, " jobs")
+    n, refs = process_parallel(loopfn, :(trees[treepaths[1]]), nprocs)
     for r in refs
         wait(r)
     end
-    @everywhere tfile_close(local_tree.file)
     return n
 end
 
+addprocs(10)
+np = nprocs()
+init()
+
+times = Any[]
+for np=1:10
+    el = @elapsed n=run(np)
+    push!(times, (np, n, el, n/el))
+end
+for t in times
+    println(t)
+end
