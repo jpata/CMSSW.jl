c0e06b72293f3066667df02758c74f1e6341b3fc
Joosep Pata <joosep@phys.hep.kbfi.ee>
2013-09-18 17:31:28 +0300

    proper cleanup fter job

--- slurm.jl
+++ slurm.jl
@@ -13,7 +13,7 @@ function launch_slurm_workers(SLURMManager, np::Integer, config::Dict)
     jobname = "julia-$(getpid())"
     
     ofname = string(jobname, ".out")
-    qsub_cmd = `salloc -n $np -p phys,prio,main srun -n $np -D $home -J $jobname -o $home/$jobname-%j-%t.out $exehome/$exename -e 'import Base.start_worker;println("hostname:",gethostname());start_worker()'` .> string(jobname, ".out") 
+    qsub_cmd = `salloc -J $jobname -n $np -p prio,main srun -n $np -D $home -J $jobname -o $home/$jobname-%j-%t.out $exehome/$exename -e 'import Base.start_worker;println("hostname:",gethostname());start_worker()'` .> string(jobname, ".out") 
     println("Allocating job: ", qsub_cmd)
     out, qsub_proc = readsfrom(qsub_cmd)
     @assert isfile(ofname)
@@ -38,9 +38,9 @@ function launch_slurm_workers(SLURMManager, np::Integer, config::Dict)
     for i=1:np
         # wait for each output stream file to get created
         fname = filename(i-1)
-        println("Waiting for file $fname")
+        #println("Waiting for file $fname")
 
-        result = timedwait(() -> (isfile(fname) && contains(open(readall, fname), "julia_worker")), 60.0)
+        result = timedwait(() -> (isfile(fname) && contains(open(readall, fname), "julia_worker")), 120.0)
         if result != :ok
             error("failed to open worker output: $i, $fname")
         end
@@ -62,31 +62,43 @@ function launch_slurm_workers(SLURMManager, np::Integer, config::Dict)
         cmd.detach = true
         io_objs[i],io_proc = readsfrom(cmd)
         io_objs[i].line_buffered = true
-        println("Job id=$id, host=$host:$port")
+        #println("Job $i id=$id, host=$host:$port")
 
-        configs[i] = merge(config, {:job => id, :task => i, :iofile => fname, :process => io_proc})
+        configs[i] = merge(config, {:job => id, :task => i, :iofile => fname, :process => io_proc, :parent_process => qsub_proc})
     end
     return (:host_port, collect(zip(hosts, ports, configs)))
 end
 
 function manage_slurm_worker(id::Integer, config::Dict, op::Symbol)
     if op == :finalize
-        kill(config[:process])
+        try
+            run(`scancel $(conf[:job])`)
+        catch
+        end
+
+        kill(config[:parent_process], 9)
+        kill(config[:process], 9)
         if isfile(config[:iofile])
+            #print(open(readall, config[:iofile]))
             rm(config[:iofile])
         end
     end
 end
 
-addprocs(25, cman=SLURMManager())
+#for i=1:10
+#    addprocs(10, cman=SLURMManager())
+#end
 
-procs = Any[]
-println("Processes created: ", nprocs())
-for i=1:nprocs()
-    try
-        push!(procs, @fetchfrom i (i, gethostname(), getpid()))
-    catch e
-        push!(procs, (i, e))
-    end
-end
-print(procs)
+#procs = Any[]
+#println("Processes created: ", nprocs())
+#for i=1:nprocs()
+#    println("Getting hostname from $i")
+#    try
+#        push!(procs, @fetchfrom i (i, gethostname(), getpid()))
+#        sleep(1.0)
+#    catch e
+#        push!(procs, (i, e))
+#    end
+#end
+#@everywhere print(1+2)
+#print(procs)
