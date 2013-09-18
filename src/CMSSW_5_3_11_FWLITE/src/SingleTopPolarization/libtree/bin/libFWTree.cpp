fde633b63b4c28135460cd522eb9aae0a05eeabb
Joosep Pata <joosep@phys.hep.kbfi.ee>
2013-09-18 17:39:30 +0300

    removed old parts duplicated in root.cc

--- src/CMSSW_5_3_11_FWLITE/src/SingleTopPolarization/libtree/bin/libFWTree.cpp
+++ src/CMSSW_5_3_11_FWLITE/src/SingleTopPolarization/libtree/bin/libFWTree.cpp
@@ -12,11 +12,6 @@
 #include <FWCore/PythonParameterSet/interface/PythonProcessDesc.h>
 #include <DataFormats/Common/interface/MergeableCounter.h>
 
-struct
-{
-    unsigned int n;
-    const float *pdata;
-} vfloat;
 extern "C" {
 
     void initialize()
@@ -26,81 +21,8 @@ extern "C" {
         gSystem->Load("libDataFormatsFWLite");
     }
 
-    void *make_handle_vfloat()
+    void *new_wrapper_vfloat()
     {
-        return new edm::Handle<std::vector<float>>();
-    }
-
-    void del_obj(void *obj)
-    {
-        delete obj;
-    }
-
-    TFile *make_tfile(const char *fname)
-    {
-        return TFile::Open(fname);
-    }
-
-    fwlite::Event *make_event(TFile *tfile)
-    {
-        return new fwlite::Event(tfile);
-    }
-
-    void event_toBegin(fwlite::Event *event)
-    {
-        event->toBegin();
-    }
-
-    void tfile_Close(void *file)
-    {
-        ((TFile *)file)->Close();
-    }
-
-    const float *event_getByLabel(fwlite::Event *evt, const char *label, edm::Handle<std::vector<float>> *handle)
-    {
-        edm::InputTag src(label);
-        const edm::EventBase &event = *evt;
-        event.getByLabel(src, *handle);
-        if (handle->isValid())
-        {
-            const std::vector<float> *prod = handle->product();
-            *n_out = prod->size();
-            return &(*prod)[0];
-        }
-        else
-        {
-            *n_out = 0;
-            return 0;
-        }
-    }
-
-    bool event_atEnd(fwlite::Event *event)
-    {
-        return event->atEnd();
-    }
-
-    const fwlite::Event *event_next(fwlite::Event *event)
-    {
-        return &(++(*event));
-    }
-
-    void hello()
-    {
-        std::cout << "libFWTree initialized!" << std::endl;
-    }
-
-    char **get_branches(TFile *f, const char *name, unsigned int *n_branches)
-    {
-        TTree *tree = (TTree *)f->Get(name);
-        TObjArray *brlist = tree->GetListOfBranches();
-        char **strs = (char **)malloc(sizeof(char *) * brlist->GetEntries());
-        for (unsigned int i = 0; i < (unsigned int)brlist->GetEntries(); i++)
-        {
-            strs[i] = (char *)malloc(sizeof(char) * 2048);
-            strcpy(strs[i], (*brlist)[i]->GetName());
-        }
-        //const char **arr[outnames.size()];
-        *n_branches = brlist->GetEntries();
-        return strs;
+        return new edm::Wrapper<std::vector<float>>();
     }
 }
