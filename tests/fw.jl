bdc7b08863b5bf3c790f72f485570ef2109f734e
Joosep Pata <joosep.pata@gmail.com>
2013-09-19 16:28:48 +0300

    several input files

--- tests/fw.jl
+++ tests/fw.jl
@@ -1,120 +1,131 @@
 require("src/root.jl")
 
 @everywhere begin
-const libfwlite = "lib/libFWTree.dylib"
+    using DataFrames
 
-function fwlite_initialize()
-    out = ccall(
-        (:initialize, libfwlite),
-        Void, (),
-    )
-    return out
-end
+    const libfwlite = "lib/libFWTree.dylib"
 
-fwlite_initialize()
-
-immutable InputTag
-    p::Ptr{Void}
-    label::Symbol
-    instance::Symbol
-    process::Symbol
-    InputTag(label::Symbol, instance::Symbol, process::Symbol) = new(
-        ccall(
-            (:new_inputtag, libfwlite),
-            Ptr{Void}, (Ptr{Uint8}, Ptr{Uint8}, Ptr{Uint8}), string(label), string(instance), string(process)
-        ), label, instance, process
-    )
-end
-
-type Events
-    tfile::TFile
-    ev::Ptr{Void}
-    handles::Dict{InputTag, Ptr{Void}}
-    tags::Dict{(Symbol, Symbol), InputTag}
-    index::Int64
-
-    function Events(s::ASCIIString)
-        tfile = tfile_open(s)
-        ev = ccall(
-        (:events, libfwlite),
-        Ptr{Void}, (TFile, ), tfile
+    function fwlite_initialize()
+        out = ccall(
+            (:initialize, libfwlite),
+            Void, (),
         )
-        return new(tfile, ev, Dict{InputTag, Ptr{Void}}(), Dict{(Symbol, Symbol), InputTag}(), 0)
+        return out
     end
-end
 
-function get_branches(ev::Events)
-    parr = ccall(
-        (:get_branches, libfwlite),
-        Ptr{CArray}, (Ptr{Void}, ), ev.ev
-    )
-    arr = unsafe_load(parr)
-    jarr = deepcopy(
-        pointer_to_array(convert(Ptr{Ptr{Uint8}}, arr.start), (convert(Int64, arr.n_elems),))
-    )
+    fwlite_initialize()
 
-    ret = Vector{Symbol}[]
-    for s in map(bytestring, jarr)
-        symbs = split(s, "_") |>  x -> map(y -> symbol(strip(y, ['.'])), x)
-        push!(ret, symbs)
-    end
-    return ret
-end
-
-function Base.length(ev::Events)
-    return ccall(
-        (:events_size, libfwlite),
-        Clong, (Ptr{Void}, ), ev.ev
-    )
-end
-
-function Base.getindex(ev::Events, n::Integer)
-    scanned = ccall(
-            (:events_to, libfwlite),
-            Bool, (Ptr{Void}, Clong), ev.ev, n
-    )
-    scanned || error("failed to scan to event $n")
-    ev.index = n
-    return ev
-end
-
-function Base.getindex(ev::Events, s::InputTag)
-    if !haskey(ev.handles, s)
-        ev.handles[s] = ccall(
-            (:new_handle_vfloat, libfwlite),
-            Ptr{Void}, ()
+    immutable InputTag
+        p::Ptr{Void}
+        label::Symbol
+        instance::Symbol
+        process::Symbol
+        InputTag(label::Symbol, instance::Symbol, process::Symbol) = new(
+            ccall(
+                (:new_inputtag, libfwlite),
+                Ptr{Void}, (Ptr{Uint8}, Ptr{Uint8}, Ptr{Uint8}), string(label), string(instance), string(process)
+            ), label, instance, process
         )
     end
-    ret = ccall(
-        (:get_by_label_vfloat, libfwlite),
-        Ptr{Void}, (Ptr{Void}, Ptr{Void}, Ptr{Void}), ev.ev, ev.handles[s], s.p
-    )
-    return ret |> to_vector
-end
 
-function Base.getindex(ev::Events, label::Symbol, instance::Symbol)
-    if !haskey(ev.tags, (label, instance))
-        ev.tags[(label, instance)] = InputTag(label, instance, symbol(""))
+    type Events
+        tfile::TFile
+        ev::Ptr{Void}
+        handles::Dict{InputTag, Ptr{Void}}
+        tags::Dict{(Symbol, Symbol), InputTag}
+        index::Int64
+
+        function Events(s::ASCIIString)
+            tfile = tfile_open(s)
+            ev = ccall(
+            (:new_event, libfwlite),
+            Ptr{Void}, (TFile, ), tfile
+            )
+            return new(tfile, ev, Dict{InputTag, Ptr{Void}}(), Dict{(Symbol, Symbol), InputTag}(), 0)
+        end
+
+        function Events(fnames::Vector{ASCIIString})
+            ev = ccall(
+            (:new_chain_event, libfwlite),
+            Ptr{Void}, (Ptr{Ptr{Uint8}}, Cuint), fnames, length(fnames)
+            )
+            return new(C_NULL, ev, Dict{InputTag, Ptr{Void}}(), Dict{(Symbol, Symbol), InputTag}(), 0)
+        end
     end
-    return ev[ev.tags[(label, instance)]]
-end
 
-function to_vector(p::Ptr{Void})
-    parr = ccall(
-            (:convert_vector, libroot),
-            Ptr{CArray}, (Ptr{Void}, ), p
+    function get_branches(ev::Events)
+        parr = ccall(
+            (:get_branches, libfwlite),
+            Ptr{CArray}, (Ptr{Void}, ), ev.ev
+        )
+        arr = unsafe_load(parr)
+        jarr = deepcopy(
+            pointer_to_array(convert(Ptr{Ptr{Uint8}}, arr.start), (convert(Int64, arr.n_elems),))
+        )
+
+        ret = Vector{Symbol}[]
+        for s in map(bytestring, jarr)
+            symbs = split(s, "_") |>  x -> map(y -> symbol(strip(y, ['.'])), x)
+            push!(ret, symbs)
+        end
+        return ret
+    end
+
+    function Base.length(ev::Events)
+        return ccall(
+            (:events_size, libfwlite),
+            Clong, (Ptr{Void}, ), ev.ev
+        )
+    end
+
+    function Base.getindex(ev::Events, n::Integer)
+        scanned = ccall(
+                (:events_to, libfwlite),
+                Bool, (Ptr{Void}, Clong), ev.ev, n-1
+        )
+        scanned || error("failed to scan to event $n")
+        ev.index = n
+        return ev
+    end
+
+    function Base.getindex(ev::Events, s::InputTag)
+        if !haskey(ev.handles, s)
+            ev.handles[s] = ccall(
+                (:new_handle_vfloat, libfwlite),
+                Ptr{Void}, ()
+            )
+        end
+        ret = ccall(
+            (:get_by_label_vfloat, libfwlite),
+            Ptr{Void}, (Ptr{Void}, Ptr{Void}, Ptr{Void}), ev.ev, ev.handles[s], s.p
+        )
+        return ret |> to_vector
+    end
+
+    function Base.getindex(ev::Events, label::Symbol, instance::Symbol)
+        if !haskey(ev.tags, (label, instance))
+            ev.tags[(label, instance)] = InputTag(label, instance, symbol(""))
+        end
+        return ev[ev.tags[(label, instance)]]
+    end
+
+    function to_vector(p::Ptr{Void})
+        p != C_NULL || error("product not found")
+
+        parr = ccall(
+                (:convert_vector, libroot),
+                Ptr{CArray}, (Ptr{Void}, ), p
+        )
+        arr = unsafe_load(parr)
+        jarr = deepcopy(pointer_to_array(convert(Ptr{Cfloat}, arr.start), (convert(Int64, arr.n_elems),)))
+        parr!= C_NULL && c_free(parr)
+        return jarr
+    end
+
+    ev = Events(
+        ["/Users/joosep/Documents/stpol/data/Sep19_qcdiso/iso/nominal/QCDMu/output_1_1_AKn.root",
+        "/Users/joosep/Documents/stpol/data/Sep19_qcdiso/iso/nominal/QCDMu/output_2_1_hi5.root"]
     )
-    arr = unsafe_load(parr)
-    # jarr = deepcopy(
-    #     pointer_to_array(convert(Ptr{Cfloat}, arr.start), (convert(Int64, arr.n_elems),))
-    # )
-    jarr = pointer_to_array(convert(Ptr{Cfloat}, arr.start), (convert(Int64, arr.n_elems),))
-    parr!= C_NULL && c_free(parr)
-    return jarr
-end
-
-
-    ev = Events("dat/test_edm.root")
 
     immutable Lepton
         pt::Float32
@@ -125,20 +136,26 @@ end
 
     hist_pt = Histogram(10, 0, 200)
 
+    leptons = Lepton[]
     function loop(n::Integer, events::Events)
-        global hist_pt
-        
+        global hist_pt::Histogram
+        global leptons
+
+        curleptons = Lepton[]
+
         events[n]
-        iso = events[:goodSignalMuonsNTupleProducer, :relIso]
-        pt = events[:goodSignalMuonsNTupleProducer, :Pt]
-        eta = events[:goodSignalMuonsNTupleProducer, :Eta]
-        phi = events[:goodSignalMuonsNTupleProducer, :Phi]
+        iso = events[:allMuonsNTP, :relIso]
+        pt = events[:allMuonsNTP, :Pt]
+        eta = events[:allMuonsNTP, :Eta]
+        phi = events[:allMuonsNTP, :Phi]
 
         for m in zip(pt, eta, phi, iso)
             mu = Lepton(m...)
-            #push!(leptons, mu)
-            fill!(hist_pt, mu.pt)
+            #push!(curleptons, mu)
+            #fill!(hist_pt, mu.pt)
+            push!(leptons, mu)
         end
+        #println("N=$(length(curleptons)), isos={",join([l.iso for l in curleptons], ","), "}")
         return true
     end
 end
@@ -147,7 +164,8 @@ rworkers = workers()
 symb = :ev
 @eval @onworkers $rworkers local_tree = eval($symb)
 
-#println("Local: ", local_tree)
+println("number of events: ", length(ev))
+
 @onworkers rworkers println("onworker $(myid()): $local_tree")
 nevents, refs = process_parallel(loop, :ev, rworkers)
 
@@ -159,7 +177,9 @@ speed = nevents/el
 println("Done: $speed events/sec")
 
 for w in rworkers
-    hist = @fetchfrom w eval(:hist_pt)
-    println(hist)
+    #hist = @fetchfrom w eval(:hist_pt)
+    #println(hist)
+    leptons = @fetchfrom w eval(:leptons)
+    println(leptons)
 end
 
