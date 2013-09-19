3b83d03e143c559b192852264703bb7d82289609
Joosep Pata <joosep.pata@gmail.com>
2013-09-19 16:28:37 +0300

    error checking for 0 pointer

--- src/root.cc
+++ src/root.cc
@@ -112,6 +112,7 @@ extern "C" {
 
     Array *convert_vector(std::vector<float> *v)
     {
+        if (v == 0) return 0;
         Array *out = (Array *)malloc(sizeof(Array));
         out->start = v->size() > 0 ? &(v->at(0)) : 0;
         out->size = sizeof(float);
