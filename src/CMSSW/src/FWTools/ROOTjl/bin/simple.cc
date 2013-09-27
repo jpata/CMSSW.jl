694dd7cfe643323816a1ea24c45b7998eec46692
Joosep Pata <joosep.pata@gmail.com>
2013-09-27 13:52:14 +0300

    cleanup

--- src/CMSSW/src/FWTools/ROOTjl/bin/simple.cc
+++ src/CMSSW/src/FWTools/ROOTjl/bin/simple.cc
@@ -1,46 +1,7 @@
 #include "fwlite.hh"
+#include "/Users/joosep/Documents/stpol/src/headers/stpol.hh"
 
-//Create a custom object with the variables that we want to accumulate across the events
-//This should contain e.g. TH1F pointer etc.
-class MyState : public State
-{
-public:
-    float sum_pt;
-};
-
-
-//Our labels are simple 3-tuples with const char*.
-static const label mu_pt = make_tuple("goodSignalMuonsNTupleProducer", "Pt", "STPOLSEL2");
-static const label mu_eta = make_tuple("goodSignalMuonsNTupleProducer", "Eta", "STPOLSEL2");
-
-//Callout must be a generic function with the signature (strict):
-// func(State*) -> bool (passed/not passed)
-//The State* must be derived of the class State (in fwlite.hh) and contains the event pointer, along with the handles
-bool callout1(State *_s)
-{
-    MyState *s = (MyState *)_s;
-
-    //Get our quantity (output can be 0 pointer, need to check explicitly)
-    const std::vector<float> *p = get_vfloat(
-                                      (h_vfloat *)(s->tags[mu_pt]), s->main_events, mu_pt
-                                  );
-    const std::vector<float> *p2 = get_vfloat(
-                                       (h_vfloat *)(s->tags[mu_eta]), s->main_events, mu_eta
-                                   );
-
-    if (p != 0)
-    {
-        s->sum_pt += p->size() > 0 ? p->at(0) : 0;
-    }
-    else
-    {
-        return false;
-    }
-    return true;
-
-}
-
-
+//Look here
 int main(int argc, char **argv)
 {
 
@@ -53,27 +14,12 @@ int main(int argc, char **argv)
 
     fw_event *events = new fw_event(fn);
 
-    //Create the state object which will be passed to the callout
-    //Contains the current event pointer
-    MyState *state = new MyState();
-    state->tags[mu_pt] = new h_vfloat();
-    state->tags[mu_eta] = new h_vfloat();
-    state->main_events = events;
-
-    //Add our callout to the list of callouts that are executed
-    state->callouts["co1"] = callout1;
-
-    //Run our code
-    //Need to do blind conversion to the generic State* pointer from our derived object
-
-    for (int i = 0; i < 5; i++)
+    for (unsigned int i = 0; i < events->size(); i++)
     {
-        state->sum_pt = 0.0;
-        do_loop((State *)state);
-    }
+        events->to(i);
 
-    //Print the output
-    cout << "sum_pt = " << state->sum_pt << endl;
+        float pt = mu::get_pt(events); //defined in stpol.hh
+    }
 
     return 0;
 }
