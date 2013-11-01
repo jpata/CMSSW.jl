4eb5263481432ab5d15705f3c2d9382b3ec51779
Joosep Pata <joosep.pata@gmail.com>
2013-11-01 08:02:53 +0200

    added license file

--- setenv.sh
+++ setenv.sh
@@ -1,23 +1,4 @@
 d=`pwd`
-#DISTR=`python -c "import platform; print (platform.system() + ' ' + ' '.join(platform.linux_distribution()))"`
-#
-#if [[ "$DISTR" == *Darwin* ]]
-#then
-#    echo "OSX detected";
-#    export SCRAM_ARCH=osx107_amd64_gcc462
-#fi
-#if [[ "$DISTR" == *"Scientific Linux 6"* ]]
-#then
-#    echo "SL6 detected";
-#    export SCRAM_ARCH=slc6_amd64_gcc472
-#fi
-#if [[ "$DISTR" == *"Scientific Linux 5"* ]]
-#then
-#    echo "SL5 detected";
-#    export SCRAM_ARCH=slc5_amd64_gcc462
-#fi
-#
-#
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $scriptdir/deps/ntuple/CMSSW
 eval `scram runtime -sh`
