6786ea887e161382f41dfd677f72da5c8446a178
Joosep Pata <joosep.pata@cern.ch>
2014-10-24 15:04:03 +0300

    fixed julia path

--- wrapper.sh
+++ wrapper.sh
@@ -22,4 +22,4 @@ scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export JULIA_PATH=$( dirname `which julia` )
 #set the CMSSW path
-source $scriptdir/setenv.sh && PATH=$PYPATH:$PATH $JULIA_PATH/usr/bin/$@
+source $scriptdir/setenv.sh && PATH=$PYPATH:$PATH $JULIA_PATH/$@
